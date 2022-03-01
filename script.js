f92ac467b7f895b9706a3b4b1be18166018b0209
MuraFederico <federicomura98@gmail.com>
2022-03-01 15:52:57 +0100

    gioco pari o dispari

--- script.js
+++ script.js
@@ -38,4 +38,50 @@ btnCheck.addEventListener('click', function(){
 
 
 
-///////////////
+/////////////// PARI O DISPARI ///////////////
+
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function isEven(nunmberToCheck){
+
+    if (nunmberToCheck % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+const btnCheck2 = document.querySelector('.check2');
+const output2 = document.querySelector('#output2');
+
+btnCheck2.addEventListener('click', function(){
+    const userGuess = document.querySelector('#user-guess').value;
+    const userNumber = parseInt(document.querySelector('#user-number').value);
+    const generatedNumber = randomNumber(1, 5);
+
+    let guessEven;
+    let finalNumber = 0;
+
+    if (userGuess == 'pari') {
+        guessEven = true;
+    }
+    if (userGuess == 'dispari') {
+        guessEven = false;
+    }
+
+    finalNumber = generatedNumber + userNumber;
+
+    if (isEven(finalNumber) == guessEven) {
+        output2.innerHTML = 'HAI INDOVINATO!'
+    } else {
+        output2.innerHTML = 'NON HAI INDOVINATO!'
+    }
+
+    console.log(generatedNumber);
+    console.log(finalNumber);
+
+})
